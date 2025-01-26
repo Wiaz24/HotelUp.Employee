3ee43e47591b7c8d8f5f4cae296cbcbe244567f4
Wiaz <wiazhub@gmail.com>
2025-01-26 11:27:11 +0100

    Employee repository

---
+++ Src/HotelUp.Employee.Persistence/Repositories/EmployeeRepository.cs
@@ -0,0 +1,31 @@
+using HotelUp.Employee.Persistence.EFCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelUp.Employee.Persistence.Repositories;
+
+public class EmployeeRepository : IEmployeeRepository
+{
+    private readonly AppDbContext _dbContext;
+
+    public EmployeeRepository(AppDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public Task<Entities.Employee?> GetByIdAsync(Guid id)
+    {
+        return _dbContext.Employees
+            .FirstOrDefaultAsync(x => x.Id == id);
+    }
+
+    public Task<List<Entities.Employee>> GetAllAsync()
+    {
+        return _dbContext.Employees.ToListAsync();
+    }
+
+    public Task AddAsync(Entities.Employee employee)
+    {
+        _dbContext.Employees.Add(employee);
+        return _dbContext.SaveChangesAsync();
+    }
+}
---
+++ Src/HotelUp.Employee.Persistence/Repositories/IEmployeeRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace HotelUp.Employee.Persistence.Repositories;
+
+public interface IEmployeeRepository
+{
+    Task<Entities.Employee?> GetByIdAsync(Guid id);
+    Task<List<Entities.Employee>> GetAllAsync();
+    Task AddAsync(Entities.Employee employee);
+}
